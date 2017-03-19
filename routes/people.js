f530ea51e25ca4e7adca33d4a1be0d6e51a1ff22
Lisa Veras <lisaveras@gmail.com>
2017-03-18 20:31:02 -0700

    minor edit

--- routes/people.js
+++ routes/people.js
@@ -49,7 +49,7 @@ router.get('/:id', (req, res, next) => {
 
 
 // Make a DELETE request to /people/1
-router.get('/:id', (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
 
 	let id = req.params.id;
 
