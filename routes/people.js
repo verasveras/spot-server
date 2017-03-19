0ac0d86c0a32e566c14fee379695aee12ce03819
Lisa Veras <lisaveras@gmail.com>
2017-03-18 20:40:55 -0700

    Made spacing uniform in people routes

--- routes/people.js
+++ routes/people.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const People = require('../db/models/people-model');
 
-// Please make the person object have the following attributes: id, name : “Sean”, favoriteCity : “New York”
-// Make a GET request to retrieve the object created in the previous request
 
 // Make a GET request to /people
 router.get('/', (req, res, next) => {
@@ -30,7 +28,7 @@ router.post('/', (req, res, next) => {
 	.catch(next);
 });
 
-// Make a GET request to /people/1
+// Make a GET request to /people/:id
 router.get('/:id', (req, res, next) => {
 
 	let id = req.params.id;
@@ -43,12 +41,21 @@ router.get('/:id', (req, res, next) => {
 	.catch(next);
 });
 
+// Make a Put request to /people/:id
+router.put('/:id', (req, res, next) => {
+
+	let id = req.params.id;
+	People.find({where: 
+		{id: id}
+	})
+	.then((person) => {
+		res.status(200).json(person);
+	})
+	.catch(next);
+});
 
 
-// Make a PUT request to /people and modify the attribute city to be “Brooklyn”
-
-
-// Make a DELETE request to /people/1
+// Make a DELETE request to /people/:id
 router.delete('/:id', (req, res, next) => {
 
 	let id = req.params.id;
@@ -62,6 +69,15 @@ router.delete('/:id', (req, res, next) => {
 	.catch(next);
 });
 
+
+Make a GET request to /people
+Make a POST request to /people
+Please make the person object have the following attributes: id, name : “Sean”, favoriteCity : “New York”
+Make a GET request to retrieve the object created in the previous request
+Make a PUT request to /people and modify the attribute city to be “Brooklyn”
+Make a GET request to /people/1
+Make a DELETE request to /people/1
+Make a GET request to /people
 
 
 
