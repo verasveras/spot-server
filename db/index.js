cf4f6ff61cf9dfca6d56a9d015a3892821237ee3
Lisa Veras <lisaveras@gmail.com>
2017-03-18 19:23:17 -0700

    db up and working on heroku

--- db/index.js
+++ db/index.js
@@ -1,7 +1,5 @@
 const Sequelize = require('sequelize');
-console.log('URL!', process.env.DATABASE_URL)
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/spotify');
 // Postgres url for Heroku OR for local.
-
 
 module.exports = db;
