5864bec4e369d7a2631e101f27a253d5ee492433
Lisa Veras <lisaveras@gmail.com>
2017-03-19 07:35:18 -0700

    Cross origin header fix

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', require('./routes'));
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(function (err, req, res, next) {
     res.status(500).send('Internal server error.');
 });
