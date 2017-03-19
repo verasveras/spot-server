119116f9d9fa8b7b32fefeaac1f95fda60a6efc3
Lisa Veras <lisaveras@gmail.com>
2017-03-18 19:27:02 -0700

    get for one id route

--- routes/people.js
+++ routes/people.js
@@ -13,20 +13,23 @@ router.get('/', (req, res, next) => {
 
 
 // Make a POST request to /people
-router.post('/', (req, res, next) => {
-	let personInfo = req.body.person;
-	People.find(personInfo)
-	.then(() => {
-		res.status(200).json(allPeople);
+router.post('/:id', (req, res, next) => {
+
+	let id = req.params.id;
+	People.find({where: 
+		{id: id}
+	})
+	.then((person) => {
+		res.status(200).json(person);
 	})
 	.catch(next);
 });
-
 
 // Please make the person object have the following attributes: id, name : “Sean”, favoriteCity : “New York”
 // Make a GET request to retrieve the object created in the previous request
 // Make a PUT request to /people and modify the attribute city to be “Brooklyn”
 // Make a GET request to /people/1
+
 // Make a DELETE request to /people/1
 // Make a GET request to /people
 
